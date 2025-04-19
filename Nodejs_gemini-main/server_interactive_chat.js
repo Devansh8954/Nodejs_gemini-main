e9f14595bfa9c7db6e4f6b946b915934b06220fb
DEVANSH TYAGI <96236567+Devansh8954@users.noreply.github.com>
2025-04-19 20:43:02 +0530

    Update server_interactive_chat.js

--- Nodejs_gemini-main/server_interactive_chat.js
+++ Nodejs_gemini-main/server_interactive_chat.js
@@ -16,7 +16,7 @@ app.use(express.json())
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Serve index.html as the default route
-app.get('/', (req, res) => {
+app.get('/', (_req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html')); 
 });
 
